bce3e17d52437fb0e66b4cff005548409f2262b7
giabaobui36 <100818088+giabaobui36@users.noreply.github.com>
2023-08-29 16:59:52 +1000

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+// Custom CSS file
+import "./styles/styles.css"
+
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <NavBar></NavBar>
       <Main></Main>
       <Footer></Footer>
---
+++ src/components/Tab.jsx
@@ -0,0 +1,12 @@
+
+
+function Tab(props) {
+    return (
+        <span>
+            <button type="button" className="btn btn-outline-dark rounded m-2">{props.category}</button>
+        </span>
+    )
+}
+
+
+export default Tab;
