73f9c5caef86a153b65e11eb10babfac1ea472e4
giabaobui36 <100818088+giabaobui36@users.noreply.github.com>
2023-09-01 12:30:10 +1000

    fix header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,33 +7,34 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 function Header() {
     return (
         // the start of the navbar
-        <nav className="navbar navbar-expand-sm rounded-pill fixed-top">
-            <div className="container-fluid">
-                <div className="col-2 p-2">
-                    <div><BubbleChartIcon />DTP</div>
-                </div>
-                <div className="collapse navber-collapse" id="#nav">
-                    <form class="border">
-						<input class="form-control me-2" type="text" placeholder="Search"/>
-						<IconButton className="float-end">
-                            <SearchIcon className="icon" />
+        <nav className="navbar navbar-expand-sm ">
+            <div className="container border border-dark rounded-pill">
+                <div className="row flex-fill">
+                    <div className="col-1">
+                        <span><BubbleChartIcon />DTP</span>
+                    </div>
+                    <div className="col-9">
+                        <span>
+                            <InputBase type="text" placeholder="Search"></InputBase>
+                            <IconButton className="float-end">
+                                <SearchIcon />
+                            </IconButton>
+                        </span>
+                    </div>
+                    <div className="col-1">
+                        <IconButton className="float-end">
+                            <AccountCircleIcon />
                         </IconButton>
-					</form>
+                    </div>
+                    <div className="col-1">
+                        <IconButton className="float-end">
+                            <ShoppingCartIcon />
+                        </IconButton>
+                    </div>
+                    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+                        <span className="navbar-toggler-icon"></span>
+                    </button> */}
                 </div>
-                <div className="col-2 rounded-pill">
-                    <span>Username</span>
-                    <IconButton className="float-end">
-                        <AccountCircleIcon/>
-                    </IconButton>
-                </div>
-                <div className = "col-1">
-                    <IconButton className="float-end">
-                        <ShoppingCartIcon/>
-                    </IconButton>
-                </div>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
-					<span className="navbar-toggler-icon"></span>
-    			</button>
             </div>
         </nav>
     )
