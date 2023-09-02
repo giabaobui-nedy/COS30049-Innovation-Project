2019e14cb430435926d97cddad23b20af44ffbe5
Duy Nguyen <141419303+khanhduy1202@users.noreply.github.com>
2023-09-02 19:30:02 +1000

    dashboard styling

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -1,17 +1,35 @@
 import { Link, Outlet} from "react-router-dom"
 import Logo from "./Logo"
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu';
 
 function UserDashBoard() {
     return (
         <div className="container">
-            <Logo/>
-            <Link to="transaction-history">
-                <p>Transaction History</p>
-            </Link>
-            <div>This is user dashboard!</div>
+            <div className="sidebar">
+                <IconButton data-bs-toggle="offcanvas" data-bs-target="#dashboard">
+                    <MenuIcon/>
+                </IconButton>
+                
+                
+            </div>
+
+            <div class="offcanvas offcanvas-start" id="dashboard">
+                <div class="offcanvas-header">
+                    <Logo/>
+                </div>
+                <div class="offcanvas-body">
+                    <button className="btn btn-outline-dark sidebar_opt">Account Details</button>
+                    <button className="btn btn-outline-dark sidebar_opt">My Assets</button>
+                    <Link to="transaction-history">
+                        <button className="btn btn-outline-dark sidebar_opt">Transaction History</button>
+                    </Link>
+                    <button className="btn btn-outline-dark sidebar_opt">...</button>
+                </div>
+            </div>
+            
             <Outlet/>
         </div>
-        
     )
 }
 
