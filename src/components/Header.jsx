395e87b6da6b9f3c6a22292dd16d0198a2a28d22
Duy Nguyen <141419303+khanhduy1202@users.noreply.github.com>
2023-09-02 15:44:01 +1000

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,47 @@
 import { InputBase, IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/SearchTwoTone';
+import AccountCircleIcon from '@mui/icons-material/AccountCircleTwoTone';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCartTwoTone';
 
 function Header() {
     return (
         // the start of the navbar
-        <nav className="navbar navbar-expand-sm">
-            <div className="container border border-dark rounded-pill">
-                <div className="row flex-fill">
-                    <div className="col-1">
-                        <Logo />
-                    </div>
-                    <div className="col-9">
-                        <span>
-                            <InputBase type="text" placeholder="Search"></InputBase>
-                            <IconButton className="float-end">
-                                <SearchIcon />
-                            </IconButton>
-                        </span>
-                    </div>
-                    <div className="col-1">
-                        <Link to="user-dashboard">
-                            <IconButton className="float-end">
-                                <AccountCircleIcon />
-                            </IconButton>
-                        </Link>
-                    </div>
-                    <div className="col-1">
+        <nav className="navbar navbar-expand-md container-fluid rounded-pill bg-dark mt-2 sticky-top">
+            <div className="container-fluid">
+                <div className="navbar-brand">
+                    <Logo />
+                </div>
+                <button className="navbar-toggler bg-white rounded-pill" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse bg-dark rounded-4" id="nav">
+                    <form class="d-inline-flex container-fluid col-9">
+                        <input className="form-control me-2" type="text" placeholder="Search" />
+                        <IconButton className="float-end text-white border border-2 border-white">
+                            <SearchIcon />
+                        </IconButton>
+                    </form>
+                    <div className="container-fluid">
+                        <span className="container-fluid bg-white ava_pill rounded-pill">
                         <Link to="cart">
                             <IconButton className="float-end">
                                 <ShoppingCartIcon />
                             </IconButton>
                         </Link>
+                        <Link to="user-dashboard">
+                            <IconButton className="float-end bg-secondary">
+                                <AccountCircleIcon />
+                            </IconButton>
+                        </Link>
+                        </span>
                     </div>
                 </div>
+                
+                    
             </div>
+                
         </nav>
     )
 }
