696d0420a067df67a175fff75d91d7fd6f711601
Duy Nguyen <141419303+khanhduy1202@users.noreply.github.com>
2023-09-02 17:41:22 +1000

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,47 @@
-import { IconButton } from "@mui/material";
+import { InputBase, IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import SearchBar from "./SearchBar";
+import SearchIcon from '@mui/icons-material/SearchTwoTone';
+import AccountCircleIcon from '@mui/icons-material/AccountCircleTwoTone';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCartTwoTone';
 
-function Header(props) {
+function Header() {
     return (
         // the start of the navbar
-        <nav className="navbar navbar-expand-sm">
-            <div className="container border border-dark rounded-pill">
-                <div className="row flex-fill">
-                    <div className="col-1">
-                        <Logo />
-                    </div>
-                    <div className="col-9">
-                        <SearchBar isSearching={props.isSearching} setIsSearching={props.setIsSearching} searchInput={props.searchInput} setSearchInput={props.setSearchInput} />
-                    </div>
-                    <div className="col-1">
-                        <Link to="user-dashboard">
-                            <IconButton className="float-end">
-                                <AccountCircleIcon />
-                            </IconButton>
-                        </Link>
-                    </div>
-                    <div className="col-1">
+        <nav className="navbar navbar-expand-md container-fluid rounded-pill bg-dark mt-2 sticky-top">
+            <div className="container-fluid">
+                <div className="navbar-brand">
+                    <Logo />
+                </div>
+                <button className="navbar-toggler bg-white rounded-pill" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse bg-dark rounded-4" id="nav">
+                    <form class="d-inline-flex container-fluid col-9">
+                        <input className="form-control me-2" type="text" placeholder="Search" />
+                        <IconButton className="float-end text-white border border-2 border-white">
+                            <SearchIcon />
+                        </IconButton>
+                    </form>
+                    <div className="container-fluid">
+                        <span className="container-fluid bg-white ava_pill rounded-pill">
                         <Link to="cart">
                             <IconButton className="float-end">
                                 <ShoppingCartIcon />
-                                <span className="badge rounded-pill text-bg-danger">{props.numberOfItems}</span>
                             </IconButton>
                         </Link>
+                        <Link to="user-dashboard">
+                            <IconButton className="float-end bg-secondary">
+                                <AccountCircleIcon />
+                            </IconButton>
+                        </Link>
+                        </span>
                     </div>
                 </div>
+                
+                    
             </div>
+                
         </nav>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar(props) {
     const categories = ["All", "Art", "Gaming", "Membership", "PFPs", "Photography", "Music"]
     return (
         <div className="container">
-            <h3>Categories</h3>
+            <h3><span className="nav_head bg-dark rounded-2 text-white ">Categories</span></h3>
             <hr></hr>
             {categories.map(category => {
                 return <Tab setChosenCategory={props.setChosenCategory} isChosen={props.chosenCategory === category} key={category} category={category} />
