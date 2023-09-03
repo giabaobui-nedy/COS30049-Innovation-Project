9c0bcff1a19339dc274fc9fd39223ff1e8146f60
Duy Nguyen <141419303+khanhduy1202@users.noreply.github.com>
2023-09-03 20:58:27 +1000

    modify cart

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -2,60 +2,35 @@ import { useState } from 'react';
 import CheckoutButton from './CheckoutButton';
 import Logo from './Logo';
 import ShoppingItem from './ShoppingItem';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
 function ShoppingCart(props) {
-  const [ascending, setAscending] = useState(true);
-
-  // Change the sort order
-  const sortedCartItems = [...props.cartItems].sort((a, b) => {
-    if (ascending) {
-      return a.itemPrice - b.itemPrice;
-    } else {
-      return b.itemPrice - a.itemPrice;
-    }
-  });
 
   return (
     <div className="container-fluid">
       <div className="logo_cont"><Logo size="50px"/></div>
       <div className="container">
-        {sortedCartItems.length === 0 ? (
+        {props.cartItems.length === 0 ? (
           <center className="h1">No Item in your cart!</center>
         ) : (
           <table className="table">
             <thead>
               <tr>
                 <th className="h4">Item description</th>
-                <th className="h4">
-                  <button
-                    className="btn btn-outline-dark"
-                    value={ascending}
-                    onClick={() => setAscending(!ascending)}
-                  >
-                    Price{' '}
-                    {ascending ? (
-                      <ArrowUpwardIcon />
-                    ) : (
-                      <ArrowDownwardIcon />
-                    )}
-                  </button>
-                </th>
+                <th className="h4">Price</th>
                 <th></th>
               </tr>
             </thead>
             <tbody>
-              {sortedCartItems.map((item) => (
+              {props.cartItems.map((item) => (
                 <ShoppingItem
                   key={item.itemId}
                   item={item}
                   price={item.itemPrice}
                   deleteItem={() => {
                     props.setCartItems(
-                      sortedCartItems.length === 1
+                      props.cartItems.length === 1
                         ? []
-                        : sortedCartItems.filter(
+                        : props.cartItems.filter(
                             (eachItem) => eachItem.itemId !== item.itemId
                           )
                     );
