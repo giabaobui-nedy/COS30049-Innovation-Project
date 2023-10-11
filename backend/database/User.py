df4bc183483476a1f44c4a1786ae539e052aee02
Duy Nguyen <103843994@student.swin.edu.au>
2023-10-11 18:06:57 +1100

    add classes

---
+++ backend/database/User.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, username, password, address, private):
+        self.username = username
+        self.password = password
+        self.address = address
+        self.private = private
