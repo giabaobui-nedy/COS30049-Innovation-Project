d06b6dab7a5669bae0c6840e8b98ac93757589fa
giabaobui36 <100818088+giabaobui36@users.noreply.github.com>
2023-10-11 17:25:06 +1100

    sample fastAPI project + backend project structure

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
+from web3 import Web3
+import os
+from solcx import compile_standard, install_solc
+import json
+
+# blockchain variables
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0x1Bf52eF94394fDC8735B07Ac12BB6f1A5C87A1aF"
+private_key = "0x582f12f96eca1f247412e173db188b529c68a1dfa1ccfd02a5430e03b33ea5c9"
+# fastAPI instance
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
