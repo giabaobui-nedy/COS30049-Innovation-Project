f75e13e68587ae272f902639f0c9214e60e740c6
Duy Nguyen <103843994@student.swin.edu.au>
2023-10-12 18:11:51 +1100

    update fixed code

--- backend/database/Database.py
+++ backend/database/Database.py
@@ -1,7 +1,6 @@
 import MySQLdb;
 import json;
-import Asset;
-import User;
+from User import User;
 
 class Database:
     def __init__(self, host, user, password, database):
@@ -26,12 +25,12 @@ class Database:
         cur = con.cursor()
         if sortBy is None:
             query = '''
-            SELECT * FROM Asset
+            SELECT * FROM Asset;
             '''
         else:
             query = f'''
             SELECT * FROM Asset
-            ORDER BY {sortBy} {sortOrder}
+            ORDER BY {sortBy} {sortOrder};
             '''
         cur.execute(query)
         rows = cur.fetchall()
@@ -53,10 +52,11 @@ class Database:
     
     def getAssetBySearch(self, con, pattern):
         cur = con.cursor()
-        query = cur.execute(f'''
+        query = f'''
         SELECT * FROM Asset
-        WHERE name LIKE '%{pattern}%' OR description LIKE '%{pattern}%'
-        ''')
+        WHERE name LIKE '%{pattern}%' OR description LIKE '%{pattern}%';
+        '''
+        cur.execute(query)
         rows = cur.fetchall()
         matchedAssets = [
             {
@@ -76,9 +76,10 @@ class Database:
 
     def getAssetByCategory(self, con, cat):
         cur = con.cursor()
-        query = cur.execute('''
-        SELECT * FROM Asset WHERE category = %s
-        ''', cat)
+        query = f'''
+        SELECT * FROM Asset WHERE category = '{cat}';
+        '''
+        cur.execute(query)
         rows = cur.fetchall()
         filteredAssets = [
             {
@@ -98,9 +99,10 @@ class Database:
 
     def getContractAddress(self, con, tokenID):
         cur = con.cursor()
-        query = cur.execute('''
-        SELECT contractAddress FROM Asset WHERE tokenID = %s                    
-        ''', tokenID)
+        query = f'''
+        SELECT contractAddress FROM Asset WHERE tokenID = '{tokenID}';                    
+        '''
+        cur.execute(query)
         contractAdd = cur.fetchone()
         if contractAdd:
             return contractAdd[0]
@@ -109,9 +111,10 @@ class Database:
     
     def getAddressOfUser(self, con, username):
         cur = con.cursor()
-        query = cur.execute('''
-        SELECT address FROM Asset WHERE username = %s                    
-        ''', username)
+        query = f'''
+        SELECT address FROM User WHERE username = '{username}';                    
+        '''
+        cur.execute(query)
         userAdd = cur.fetchone()
         if userAdd:
             return userAdd[0]
@@ -122,7 +125,7 @@ class Database:
         cur = con.cursor()
         query = cur.execute('''
         INSERT INTO Asset
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)                    
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s);                    
         ''', (asset.tokenID, asset.name, asset.category, asset.price, asset.description, asset.currentOwner, asset.contractAddress, asset.imgUrl))
         con.commit()
         if query > 0:
@@ -134,27 +137,10 @@ class Database:
         cur = con.cursor()
         query = cur.execute('''
         INSERT INTO User
-        VALUES (%s, %s, %s, %s)                    
+        VALUES (%s, %s, %s, %s);                    
         ''', (user.username, user.password, user.address, user.privateKey))
         con.commit()
         if query > 0:
             print("User added successfully.")
         else:
             print("Failed to add user.")
-
-    
-
-
-
-
-dtb = Database("feenix-mariadb.swin.edu.au", "s103843994", "120203", "s103843994_db")
-con = dtb.connect()
-dtb.getAllAssets(con, "price")
-dtb.getAssetBySearch(con, "star")
-dtb.getAssetByCategory(con, "photography")
-dtb.getAddressOfUser(con, "Zuychin")
-dtb.getContractAddress(con, 6)
-
-user = User("Someone", "pwd", "479d58d7EA5DA499f7Baf6EAB5B54A6D8c014F15", "flkjasdflkjwo")
-dtb.addUser(con, user)
-dtb.disconnect(con)
--- backend/database/test.py
+++ backend/database/test.py
@@ -1,14 +1,15 @@
-import Database;
-import Asset;
-import User;
+from User import User;
+from Database import Database;
+
 dtb = Database("feenix-mariadb.swin.edu.au", "s103843994", "120203", "s103843994_db")
 con = dtb.connect()
-dtb.getAllAssets(con, "price")
-dtb.getAssetBySearch(con, "star")
-dtb.getAssetByCategory(con, "photography")
-dtb.getAddressOfUser(con, "Zuychin")
-dtb.getContractAddress(con, 6)
-
+assets = dtb.getAllAssets(con, "price")
+assets2 = dtb.getAssetBySearch(con, "star")
+assets3 = dtb.getAssetByCategory(con, "photography")
+address = dtb.getAddressOfUser(con, "Zuychin")
+print(address)
+contAdd = dtb.getContractAddress(con, 6)
+print(contAdd)
 user = User("Someone", "pwd", "479d58d7EA5DA499f7Baf6EAB5B54A6D8c014F15", "flkjasdflkjwo")
-dtb.addUser(con, user)
+#dtb.addUser(con, user)
 dtb.disconnect(con)
