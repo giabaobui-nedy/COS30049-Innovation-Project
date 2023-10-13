acd0fa6d0ba08e86dbaa853c5846832acfc4876f
giabaobui36 <100818088+giabaobui36@users.noreply.github.com>
2023-10-13 12:20:44 +1100

    get web3 instance working

--- backend/web3Project/Web3Instance.py
+++ backend/web3Project/Web3Instance.py
@@ -3,16 +3,9 @@ from solcx import compile_standard, install_solc
 import json
 
 
-class Web3Instance:
-    w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-    chainId = 1337
-
-    def __init__(self):
-        print("Web3 is ready!")
-
-
-    def compileSmartContract(self):
-        print("The code goes here!")
+def compileSmartContract():
+    try:
+        print("Start to compile!")
         # read the system smart contract
         with open("./AssetSC.sol", "r") as file:
             storage_file = file.read()
@@ -33,35 +26,44 @@ class Web3Instance:
         # write compile info to the json file
         with open("compiled_code.json", "w") as file:
             json.dump(compiled_sol, file)
-        pass
+    except:
+        print("Error when compiling the smart contract")
+    finally:
+        print("End compiling")
 
-    def getAbi(self):
-        # get abi
-        with open("compiled_code.json", "r") as file:
-            compiled_sol = json.load(file)
-        abi = compiled_sol["contracts"]["AssetSC.sol"]["AssetSC"]["abi"]
-        return abi
 
-    def getBytecode(self):
-        # get bytecode
-        with open("compiled_code.json", "r") as file:
-            compiled_sol = json.load(file)
-        bytecode = compiled_sol["contracts"]["AssetSC.sol"]["AssetSC"]["evm"]["bytecode"]["object"]
-        return bytecode
+def getAbi():
+    # get abi
+    with open("compiled_code.json", "r") as file:
+        compiled_sol = json.load(file)
+    abi = compiled_sol["contracts"]["AssetSC.sol"]["AssetSC"]["abi"]
+    return abi
+
+
+def getBytecode():
+    # get bytecode
+    with open("compiled_code.json", "r") as file:
+        compiled_sol = json.load(file)
+    bytecode = compiled_sol["contracts"]["AssetSC.sol"]["AssetSC"]["evm"]["bytecode"]["object"]
+    return bytecode
+
+
+class Web3Instance:
+    w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+    chainId = 1337
+
+    def __init__(self):
+        print("Web3 is ready!")
 
     def deployAssetToBlockChain(self, ownerAddress, privateKey, tokenId):
+        print("Start deploying asset")
         # 1. getAbi() getBytecode() from local file (as it has one version)
-        # 2. get the smart contract
-        # 3. provide the token ID to the constructor
-        # 4. get the nonce of the address
-        # 5. provide the owner address & nonce to the contract
-        print("The code goes here!")
+        abi = getAbi()
+        bytecode = getBytecode()
+        # 2. get the nonce of the address
         nonce = self.w3.eth.get_transaction_count(ownerAddress)
-
-        abi = self.getAbi()
-        bytecode = self.getBytecode()
-        # deploy the smart contract
-        # create transaction (get the smart contract from the Blockchain)
+        # 3. get the smart contract & create transaction
+        # 4. provide the token ID to the constructor & provide detail to the transaction
         AssetSC = self.w3.eth.contract(abi=abi, bytecode=bytecode)
         transaction = AssetSC.constructor(tokenId).build_transaction(
             {
@@ -76,4 +78,15 @@ class Web3Instance:
         signed_txn = self.w3.eth.account.sign_transaction(transaction, private_key=privateKey)
         tx_hash = self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
         tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        print("deploy successfully")
         return tx_receipt.contractAddress
+
+
+def Main():
+    w3Instance = Web3Instance()
+    print(w3Instance.deployAssetToBlockChain("0x7A2F46c7CAB756beA84BE395b1075a2aC79f73aB",
+                                             "0x2e99aa9b93e0ca18764c4a2d560eefe34ff84c36d9979c3c9e734191242f1c73",
+                                             1))
+
+
+Main()
